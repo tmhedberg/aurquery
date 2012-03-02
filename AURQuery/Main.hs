65d95ff5f82b5f91a5fc907c244de052ec163d6f
Taylor Hedberg <t@tmh.cc>
2012-03-02 14:52:06 -0500

    Option to display usage information & exit

--- AURQuery/Main.hs
+++ AURQuery/Main.hs
@@ -4,22 +4,33 @@ import Data.Maybe
 
 import System.Console.GetOpt
 import System.Environment
+import System.Exit
 
 import AURQuery.Local
 import AURQuery.Remote
 import AURQuery.Types
 
+options = [ (Option
+                ['d']
+                ["aurdir"]
+                (ReqArg id "DIR")
+                "Directory where you extract AUR tarballs (default: $HOME/aur)")
+          , (Option ['h', '?']
+                    ["help"]
+                    (NoArg "HELP")
+                    "Display usage information")
+          ]
+
 main = do
-    (opts, _, _) <-
-        fmap
-            (getOpt
-                Permute
-                [(Option ['d']
-                         ["aurdir"]
-                         (ReqArg id "DIR")
-                         "Directory where you extract AUR tarballs")])
-            getArgs
-    ipkgs <- installedPkgs $ listToMaybe opts
+    (opts, _, _) <- fmap (getOpt Permute options) getArgs
+    when
+        ("HELP" `elem` opts)
+        (getProgName
+            >>= (\pn ->
+                    putStr $
+                        usageInfo ("Usage: " ++ pn ++ " [<options>]") options)
+            >> exitSuccess)
+    ipkgs <- installedPkgs $ listToMaybe $ filter (/="HELP") opts
     forM_ ipkgs $ \(Pkg pname lv) -> do
         mrp <- remotePkg pname
         case mrp of
